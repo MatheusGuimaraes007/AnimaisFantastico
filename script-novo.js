61d4c123ae7afaaac859e989cd75ea1a65a86e51
Matheus Guimarães <matheusguimaraes668@gmail.com>
2023-05-11 11:59:05 -0300

    Trocado de lugar, scroll animacao

--- script-novo.js
+++ script-novo.js
@@ -1,4 +1,3 @@
-import initAnimacaoScroll from '/animacao-scroll.js';
 import ScrollSuave from '/scrol-suave.js';
 import initAcordion from '/acordion.js';
 import iniTabNav from '/tab-nav.js';
@@ -8,6 +7,7 @@ import dropDownMenu from '/dropdownmenu.js';
 import initMenuMobile from '/menumobile.js';
 import initAnimaNumeros from '/anima-numeros.js';
 import initFuncionamento from '/horariofuncionamento.js';
+import initAnimacaoScroll from '/animacao-scroll.js';
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
