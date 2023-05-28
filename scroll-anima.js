6c2f3ede03add7cdf6c9af98dbc4fcfb3ed7cf7e
Matheus Guimarães <matheusguimaraes668@gmail.com>
2023-05-28 12:54:13 -0300

    refeito scroll

--- scroll-anima.js
+++ scroll-anima.js
@@ -2,22 +2,42 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.AnimaScroll = this.AnimaScroll.bind(this);
-  }
 
-  AnimaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+    this.CheckDistance = this.CheckDistance.bind(this);
+  }
+  //pega a distancia de cada item em relação ao topo do site
+  GetDistance() {
+    this.distance = [...this.sections].map(section => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+  //verifica a distancia de cada objeto em relacao em cada obejto do site
+  CheckDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     })
   }
 
   init() {
-    window.addEventListener('scroll', this.AnimaScroll);
+    if (this.sections.length) {
+      this.GetDistance();
+      this.CheckDistance();
+      window.addEventListener('scroll', this.CheckDistance);
+    }
+    return this;
+  }
+  //remove o event de scroll
+  stop() {
+    window.removeEventListener('scroll', this.CheckDistance);
   }
 }
+
+  
